26b06826141618b78286feb04e2f44d20bd9dbe9
datnd299 <35355001+datnd299@users.noreply.github.com>
2019-04-03 12:30:59 +0700

    fd

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+var dbPassword = 'mongodb+srv://datnd:'+ encodeURIComponent('29091997q') + '@cluster0-shard-00-00-omy66.mongodb.net/test?retryWrites=true';
+//var dbPassword = 'mongodb://localhost/Webantoan'
+module.exports = {
+    mongoURI: dbPassword
+};
