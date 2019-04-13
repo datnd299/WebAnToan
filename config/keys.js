4bfaacc5c7857280abdb6ce6fde973a63218df34
datnd299 <35355001+datnd299@users.noreply.github.com>
2019-04-13 17:21:11 +0700

    fds

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-var dbPassword = 'mongodb+srv://datnd:29091997q@cluster0-6qp73.mongodb.net/test?retryWrites=true';
-//var dbPassword = 'mongodb://localhost/Webantoan'
-module.exports = {
-    mongoURI: dbPassword
-};
